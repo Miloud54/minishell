af15f6e33c04da4f93d8cec9029daafa20cfcc0f
Emilie Didier <edidier@f2r9s13.clusters.42paris.fr>
2025-09-19 16:06:50 +0200

    merge de nos 2 .h

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edidier <edidier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 17:10:38 by edidier           #+#    #+#             */
-/*   Updated: 2025/09/19 15:56:28 by bde-la-p         ###   ########.fr       */
+/*   Updated: 2025/09/19 16:06:24 by edidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,32 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-typedef struct	s_token
+#define TOKEN_REDIR_IN 1      // "<"
+#define TOKEN_HEREDOC 2       // "<<"
+#define TOKEN_REDIR_OUT 3     // ">"
+#define TOKEN_APPEND 4        // ">>"
+#define TOKEN_PIPE 5          // "|"
+#define TOKEN_CMD 6           // Commande
+#define TOKEN_WORD 7
+
+
+typedef struct s_token
 {
 	char			*value;
 	int				type;
+	struct s_token	*prev;
 	struct s_token	*next;
-}	t_token;
+}					t_token;
 
-typedef struct	s_cmd
+typedef struct s_cmd
 {
 	char			**args;
-	int				input_fd;
-	int				output_fd;
-	struct	s_cmd	*next;
-}	t_cmd;
+	char			*input_file;
+	char			*output_file;
+	int				append; // 1 si >>, 0 si >
+	struct s_cmd	*prev;
+	struct s_cmd	*next;
+}					t_cmd;
 
 
 typedef struct s_ms
